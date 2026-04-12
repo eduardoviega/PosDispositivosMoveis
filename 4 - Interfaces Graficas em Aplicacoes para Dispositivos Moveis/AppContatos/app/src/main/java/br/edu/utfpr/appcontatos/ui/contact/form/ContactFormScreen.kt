1663220cd5933a2bc040f52f0ce9cb06ddd403b6
eduardoviega <eduardoviega12@gmail.com>
2026-04-12 19:02:01 -0300

    AppContatos - State e ViewModel no formulario

--- 4 - Interfaces Graficas em Aplicacoes para Dispositivos Moveis/AppContatos/app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/form/ContactFormScreen.kt
+++ 4 - Interfaces Graficas em Aplicacoes para Dispositivos Moveis/AppContatos/app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/form/ContactFormScreen.kt
@@ -1,41 +1,76 @@
 package br.edu.utfpr.appcontatos.ui.contact.form
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import br.edu.utfpr.appcontatos.data.ContactTypeEnum
+import br.edu.utfpr.appcontatos.ui.contact.form.composables.FormCheckbox
+import br.edu.utfpr.appcontatos.ui.contact.form.composables.FormDatePicker
+import br.edu.utfpr.appcontatos.ui.contact.form.composables.FormFieldRow
+import br.edu.utfpr.appcontatos.ui.contact.form.composables.FormRadioButton
+import br.edu.utfpr.appcontatos.ui.contact.form.composables.FormTextField
+import br.edu.utfpr.appcontatos.ui.shared.composables.ContactAvatar
+import br.edu.utfpr.appcontatos.ui.shared.composables.DefaultErrorState
+import br.edu.utfpr.appcontatos.ui.shared.composables.DefaultLoadingState
 import br.edu.utfpr.appcontatos.ui.theme.AppContatosTheme
 
 @Composable
 fun ContactFormScreen(
     modifier: Modifier = Modifier,
-    onBackPressed: () -> Unit
+    onBackPressed: () -> Unit,
+    viewModel: ContactFormViewModel = viewModel()
 ) {
-    Scaffold(
-        modifier = modifier.fillMaxSize(),
-        topBar = {
-            AppBar(
-                isNewContact = true,
-                onBackPressed = onBackPressed
+    val contentModifier: Modifier = modifier.fillMaxSize()
+    if (viewModel.uiState.isLoading) {
+        DefaultLoadingState(modifier = contentModifier)
+    } else if (viewModel.uiState.hasErrorLoading) {
+        DefaultErrorState(
+            modifier = contentModifier,
+            onTryAgainPressed = viewModel::loadContact
+        )
+    } else {
+        Scaffold(
+            modifier = contentModifier,
+            topBar = {
+                AppBar(
+                    isNewContact = true,
+                    onBackPressed = onBackPressed
+                )
+            }
+        ) { paddingValues ->
+            FormContent(
+                modifier = Modifier.padding(paddingValues),
+                formState = viewModel.uiState.formState,
+                onFormEvent = viewModel::onFormEvent
             )
         }
-    ) { paddingValues ->
-        Text(
-            text = "Alterar",
-            modifier = Modifier.padding(paddingValues)
-        )
     }
 }
 
@@ -75,6 +110,165 @@ private fun AppBarPreview(
         AppBar(
             isNewContact = isNewContact,
             onBackPressed = {}
+        )
+    }
+}
+
+@Composable
+private fun FormContent(
+    modifier: Modifier = Modifier,
+    formState: FormState,
+    onFormEvent: (FormEvent) -> Unit
+) {
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .imePadding()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val formTextFieldModifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+        ContactAvatar(
+            modifier = Modifier.padding(16.dp),
+            firstName = formState.firstName.value,
+            lastName = formState.lastName.value,
+            size = 150.dp,
+            textStyle = MaterialTheme.typography.displayLarge
+        )
+        FormFieldRow(
+            label = "Nome",
+            imageVector = Icons.Filled.Person
+        ) {
+            FormTextField(
+                modifier = formTextFieldModifier,
+                label = "Nome",
+                value = formState.firstName.value,
+                errorMessage = formState.firstName.errorMessage,
+                onValueChange = { newValue ->
+                    onFormEvent(FormEvent.UpdateFirstName(newValue))
+                },
+                keyboardCapitalization = KeyboardCapitalization.Words
+            )
+        }
+        FormFieldRow(
+            label = "Sobrenome"
+        ) {
+            FormTextField(
+                modifier = formTextFieldModifier,
+                label = "Sobrenome",
+                value = formState.lastName.value,
+                errorMessage = formState.lastName.errorMessage,
+                onValueChange = { newValue ->
+                    onFormEvent(FormEvent.UpdateLastName(newValue))
+                },
+                keyboardCapitalization = KeyboardCapitalization.Words
+            )
+        }
+        FormFieldRow(
+            label = "Telefone",
+            imageVector = Icons.Filled.Phone
+        ) {
+            FormTextField(
+                modifier = formTextFieldModifier,
+                label = "Telefone",
+                value = formState.phoneNumber.value,
+                errorMessage = formState.phoneNumber.errorMessage,
+                onValueChange = { newValue ->
+                    onFormEvent(FormEvent.UpdatePhoneNumber(newValue))
+                },
+                keyboardType = KeyboardType.Phone
+            )
+        }
+        FormFieldRow(
+            label = "E-mail",
+            imageVector = Icons.Filled.Mail
+        ) {
+            FormTextField(
+                modifier = formTextFieldModifier,
+                label = "E-mail",
+                value = formState.email.value,
+                errorMessage = formState.email.errorMessage,
+                onValueChange = { newValue ->
+                    onFormEvent(FormEvent.UpdateEmail(newValue))
+                },
+                keyboardType = KeyboardType.Email
+            )
+        }
+        FormFieldRow(
+            label = "Data de aniversário"
+        ) {
+            FormDatePicker(
+                modifier = formTextFieldModifier,
+                label = "Data de aniversário",
+                value = formState.birthDate.value,
+                errorMessage = formState.birthDate.errorMessage,
+                onValueChange = { newValue ->
+                    onFormEvent(FormEvent.UpdateBirthDate(newValue))
+                }
+            )
+        }
+        FormFieldRow(
+            label = "Valor patrimonial",
+            imageVector = Icons.Filled.AttachMoney
+        ) {
+            FormTextField(
+                modifier = formTextFieldModifier,
+                label = "Valor patrimonial",
+                value = formState.assetValue.value,
+                errorMessage = formState.assetValue.errorMessage,
+                onValueChange = { newValue ->
+                    onFormEvent(FormEvent.UpdateAssetValue(newValue))
+                },
+                keyboardType = KeyboardType.Number
+            )
+        }
+        val choiceOptionsModifier = Modifier.padding(8.dp)
+        FormFieldRow(
+            label = "Favorito"
+        ) {
+            FormCheckbox(
+                modifier = choiceOptionsModifier,
+                label = "Favorito",
+                checked = formState.isFavorite.value,
+                onCheckedChange = { newValue ->
+                    onFormEvent(FormEvent.UpdateIsFavorite(newValue))
+                }
+            )
+        }
+        FormFieldRow(
+            label = "Tipo"
+        ) {
+            FormRadioButton(
+                modifier = choiceOptionsModifier,
+                label = "Pessoal",
+                value = ContactTypeEnum.PERSONAL,
+                groupValue = formState.type.value,
+                onValueChanged = { newValue ->
+                    onFormEvent(FormEvent.UpdateType(newValue))
+                }
+            )
+            FormRadioButton(
+                modifier = choiceOptionsModifier,
+                label = "Profissional",
+                value = ContactTypeEnum.PROFESSIONAL,
+                groupValue = formState.type.value,
+                onValueChanged = { newValue ->
+                    onFormEvent(FormEvent.UpdateType(newValue))
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun FormContentPreview() {
+    AppContatosTheme {
+        FormContent(
+            formState = FormState(),
+            onFormEvent = {}
         )
     }
 }
---
+++ 4 - Interfaces Graficas em Aplicacoes para Dispositivos Moveis/AppContatos/app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/form/ContactFormViewModel.kt
@@ -0,0 +1,205 @@
+package br.edu.utfpr.appcontatos.ui.contact.form
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.edu.utfpr.appcontatos.data.ContactDatasource
+import br.edu.utfpr.appcontatos.data.ContactTypeEnum
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.math.BigDecimal
+import java.time.LocalDate
+import kotlin.random.Random
+
+class ContactFormViewModel(
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    private val contactId: Int = savedStateHandle.get<String>("id")?.toIntOrNull() ?: 0
+    var uiState: ContactFormState by mutableStateOf(
+        ContactFormState(
+            contactId = contactId
+        )
+    )
+
+    init {
+        if (!uiState.isNewContact) {
+            loadContact()
+        }
+    }
+
+    fun loadContact() {
+        uiState = uiState.copy(
+            isLoading = true,
+            hasErrorLoading = false
+        )
+        viewModelScope.launch {
+            delay(2000)
+            val contact = ContactDatasource.instance.findById(contactId)
+            val hasError = Random.nextBoolean()
+            uiState = if (contact == null || hasError) {
+                uiState.copy(
+                    isLoading = false,
+                    hasErrorLoading = true
+                )
+            } else {
+                uiState.copy(
+                        isLoading = false,
+                        contact = contact,
+                        formState = FormState(
+                            firstName = FormField(contact.firstName),
+                            lastName = FormField(contact.lastName),
+                            phoneNumber = FormField(contact.phoneNumber),
+                            email = FormField(contact.email),
+                            isFavorite = FormField(contact.isFavorite),
+                            assetValue = FormField(contact.assetValue.toString()),
+                            birthDate = FormField(contact.birthDate),
+                            type = FormField(contact.type)
+                        )
+                    )
+            }
+        }
+    }
+
+    fun onFormEvent(event: FormEvent) {
+        when (event) {
+            is FormEvent.UpdateAssetValue -> onAssetValueChanged(event.newValue)
+            is FormEvent.UpdateBirthDate -> onBirthDateChanged(event.newValue)
+            is FormEvent.UpdateEmail -> onEmailChanged(event.newValue)
+            is FormEvent.UpdateFirstName -> onFirstNameChanged(event.newValue)
+            is FormEvent.UpdateIsFavorite -> onIsFavoriteChanged(event.newValue)
+            is FormEvent.UpdateLastName -> onLastNameChanged(event.newValue)
+            is FormEvent.UpdatePhoneNumber -> onPhoneNumberChanged(event.newValue)
+            is FormEvent.UpdateType -> onTypeChanged(event.newValue)
+        }
+    }
+
+    private fun onFirstNameChanged(newValue: String) {
+        if (uiState.formState.firstName.value == newValue) return
+
+        uiState = uiState.copy(
+            formState = uiState.formState.copy(
+                firstName = FormField(
+                    value = newValue,
+                    errorMessage = validateFirstName(newValue)
+                )
+            )
+        )
+    }
+
+    private fun validateFirstName(value: String): String =
+        if (value.isBlank()) {
+            "O nome é obrigatório"
+        } else {
+            ""
+        }
+
+    private fun onLastNameChanged(newValue: String) {
+        if (uiState.formState.lastName.value == newValue) return
+
+        uiState = uiState.copy(
+            formState = uiState.formState.copy(
+                lastName = FormField(newValue)
+            )
+        )
+    }
+
+    private fun onPhoneNumberChanged(newValue: String) {
+        if (uiState.formState.phoneNumber.value == newValue) return
+
+        uiState = uiState.copy(
+            formState = uiState.formState.copy(
+                phoneNumber = FormField(
+                    value = newValue,
+                    errorMessage = validatePhoneNumber(newValue)
+                )
+            )
+        )
+    }
+
+    private fun validatePhoneNumber(value: String): String =
+        if (value.isBlank()
+            || (value.length in 10..11
+                    && !value.contains(Regex("\\D")))) {
+            ""
+        } else {
+            "Informe um telefone válido (apenas dígitos)"
+        }
+
+    private fun onEmailChanged(newValue: String) {
+        if (uiState.formState.email.value == newValue) return
+
+        uiState = uiState.copy(
+            formState = uiState.formState.copy(
+                email = FormField(
+                    value = newValue,
+                    errorMessage = validateEmail(newValue)
+                )
+            )
+        )
+    }
+
+    private fun validateEmail(value: String): String =
+        if (value.isNotBlank()
+            && !Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$").matches(value)) {
+            "Informe um e-mail válido"
+        } else {
+            ""
+        }
+
+    private fun onIsFavoriteChanged(newValue: Boolean) {
+        if (uiState.formState.isFavorite.value == newValue) return
+
+        uiState = uiState.copy(
+            formState = uiState.formState.copy(
+                isFavorite = FormField(newValue)
+            )
+        )
+    }
+
+    private fun onAssetValueChanged(newValue: String) {
+        if (uiState.formState.assetValue.value == newValue) return
+
+        uiState = uiState.copy(
+            formState = uiState.formState.copy(
+                assetValue = FormField(
+                    value = newValue,
+                    errorMessage = validateAssetValue(newValue)
+                )
+            )
+        )
+    }
+
+    private fun validateAssetValue(value: String): String {
+        if (value.isBlank()) return ""
+
+        try {
+            BigDecimal(value)
+            return ""
+        } catch (_: NumberFormatException) {
+            return "Informe um valor válido"
+        }
+    }
+
+    private fun onBirthDateChanged(newValue: LocalDate) {
+        if (uiState.formState.birthDate.value == newValue) return
+
+        uiState = uiState.copy(
+            formState = uiState.formState.copy(
+                birthDate = FormField(newValue)
+            )
+        )
+    }
+
+    private fun onTypeChanged(newValue: ContactTypeEnum) {
+        if (uiState.formState.type.value == newValue) return
+
+        uiState = uiState.copy(
+            formState = uiState.formState.copy(
+                type = FormField(newValue)
+            )
+        )
+    }
+}
