c82ba96740458f59df10bf7b70d6ad5524e77da0
eduardoviega <eduardoviega12@gmail.com>
2026-04-12 22:05:59 -0300

    Atividade Final - Interfaces Graficas

---
+++ 4 - Interfaces Graficas em Aplicacoes para Dispositivos Moveis/Atividade Final - Contas/app/src/main/java/br/edu/utfpr/trabalhofinal/ui/lancamento/lista/ListaLancamentosScreen.kt
@@ -0,0 +1,307 @@
+package br.edu.utfpr.trabalhofinal.ui.lancamento.lista
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.ThumbDownOffAlt
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import br.edu.utfpr.trabalhofinal.R
+import br.edu.utfpr.trabalhofinal.data.Lancamento
+import br.edu.utfpr.trabalhofinal.data.TipoLancamentoEnum
+import br.edu.utfpr.trabalhofinal.ui.shared.composables.Carregando
+import br.edu.utfpr.trabalhofinal.ui.shared.composables.ErroAoCarregar
+import br.edu.utfpr.trabalhofinal.ui.theme.TrabalhoFinalTheme
+import br.edu.utfpr.trabalhofinal.utils.calcularProjecao
+import br.edu.utfpr.trabalhofinal.utils.calcularSaldo
+import br.edu.utfpr.trabalhofinal.utils.formatar
+import java.math.BigDecimal
+import java.time.LocalDate
+
+private val CorDespesa = Color(0xFFCF5355)
+private val CorReceita = Color(0xFF00984E)
+
+@Composable
+fun ListaLancamentosScreen(
+    modifier: Modifier = Modifier,
+    onAdicionarPressed: () -> Unit,
+    onLancamentoPressed: (Lancamento) -> Unit,
+    viewModel: ListaLancamentosViewModel = viewModel()
+) {
+    val contentModifier: Modifier = modifier.fillMaxSize()
+    if (viewModel.state.carregando) {
+        Carregando(modifier = contentModifier)
+    } else if (viewModel.state.erroAoCarregar) {
+        ErroAoCarregar(
+            modifier = contentModifier,
+            onTryAgainPressed = viewModel::carregarLancamentos,
+        )
+    } else {
+        Scaffold(
+            modifier = contentModifier,
+            topBar = { AppBar(onAtualizarPressed = viewModel::carregarLancamentos) },
+            bottomBar = { BottomBar(lancamentos = viewModel.state.lancamentos) },
+            floatingActionButton = {
+                FloatingActionButton(onClick = onAdicionarPressed) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = stringResource(R.string.adicionar)
+                    )
+                }
+            }
+        ) { paddingValues ->
+            val modifierWithPadding = Modifier.padding(paddingValues)
+            if (viewModel.state.lancamentos.isEmpty()) {
+                ListaVazia(modifier = modifierWithPadding.fillMaxSize())
+            } else {
+                List(
+                    modifier = modifierWithPadding,
+                    lancamentos = viewModel.state.lancamentos,
+                    onLancamentoPressed = onLancamentoPressed
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AppBar(
+    modifier: Modifier = Modifier,
+    onAtualizarPressed: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(stringResource(R.string.lancamentos)) },
+        modifier = modifier.fillMaxWidth(),
+        colors = TopAppBarDefaults.topAppBarColors(
+            titleContentColor = MaterialTheme.colorScheme.primary,
+            navigationIconContentColor = MaterialTheme.colorScheme.primary,
+            actionIconContentColor = MaterialTheme.colorScheme.primary,
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        actions = {
+            IconButton(onClick = onAtualizarPressed) {
+                Icon(
+                    imageVector = Icons.Filled.Refresh,
+                    contentDescription = stringResource(R.string.atualizar)
+                )
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AppBarPreview() {
+    TrabalhoFinalTheme {
+        AppBar(
+            onAtualizarPressed = {}
+        )
+    }
+}
+
+@Composable
+private fun ListaVazia(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.padding(top = 8.dp, start = 8.dp, end = 8.dp),
+            text = stringResource(R.string.lista_vazia_title),
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            modifier = Modifier.padding(top = 8.dp, start = 8.dp, end = 8.dp),
+            text = stringResource(R.string.lista_vazia_subtitle),
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ListaVaziaPreview() {
+    TrabalhoFinalTheme {
+        ListaVazia()
+    }
+}
+
+@Composable
+private fun List(
+    modifier: Modifier = Modifier,
+    lancamentos: List<Lancamento>,
+    onLancamentoPressed: (Lancamento) -> Unit
+) {
+    fun valorComSinal(lacamento: Lancamento): BigDecimal {
+        if (lacamento.tipo == TipoLancamentoEnum.DESPESA) return lacamento.valor.negate()
+        return lacamento.valor
+    }
+
+    LazyColumn(modifier = modifier) {
+        items(lancamentos) { lancamento ->
+            val corItem = if (lancamento.tipo == TipoLancamentoEnum.DESPESA) CorDespesa else CorReceita
+
+            ListItem(
+                modifier = Modifier.clickable { onLancamentoPressed(lancamento) },
+                headlineContent = {
+                    Text(text = lancamento.descricao)
+                },
+                supportingContent = {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(text = lancamento.data.formatar())
+                        Spacer(modifier = Modifier.weight(1f))
+                        Text(
+                            text = valorComSinal(lancamento).formatar(),
+                            color = corItem
+                        )
+                    }
+                },
+                leadingContent = {
+                    val descricao = if (lancamento.paga) R.string.lancamento_pago else R.string.lancamento_pendente
+                    Icon(
+                        imageVector = if (lancamento.paga) Icons.Filled.ThumbUp else Icons.Filled.ThumbDownOffAlt,
+                        contentDescription = stringResource(descricao),
+                        tint = corItem
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ListPreview() {
+    TrabalhoFinalTheme {
+        List(
+            lancamentos = gerarLancamentos(),
+            onLancamentoPressed = {}
+        )
+    }
+}
+
+@Composable
+private fun BottomBar(
+    modifier: Modifier = Modifier,
+    lancamentos: List<Lancamento>
+) {
+    fun corPorValor(valor: BigDecimal): Color {
+        return if (valor < BigDecimal.ZERO) CorDespesa else CorReceita
+    }
+
+    Column(
+        modifier = modifier
+            .background(color = MaterialTheme.colorScheme.secondaryContainer),
+    ) {
+        Totalizador(
+            modifier = Modifier.padding(top = 20.dp),
+            titulo = stringResource(R.string.saldo),
+            valor = lancamentos.calcularSaldo(),
+            textColor = corPorValor(lancamentos.calcularSaldo())
+        )
+        Totalizador(
+            modifier = Modifier.padding(bottom = 20.dp),
+            titulo = stringResource(R.string.previsao),
+            valor = lancamentos.calcularProjecao(),
+            textColor = corPorValor(lancamentos.calcularProjecao())
+        )
+    }
+}
+
+@Composable
+fun Totalizador(
+    modifier: Modifier = Modifier,
+    titulo: String,
+    valor: BigDecimal,
+    textColor: Color
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End,
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            textAlign = TextAlign.End,
+            text = titulo
+        )
+        Spacer(Modifier.size(10.dp))
+        Text(
+            modifier = Modifier.width(100.dp),
+            textAlign = TextAlign.End,
+            text = valor.formatar(),
+            color = textColor
+        )
+        Spacer(Modifier.size(20.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BottomBarPreview() {
+    TrabalhoFinalTheme {
+        BottomBar(
+            lancamentos = gerarLancamentos()
+        )
+    }
+}
+
+private fun gerarLancamentos(): List<Lancamento> = listOf(
+    Lancamento(
+        descricao = "Salário",
+        valor = BigDecimal("5000.0"),
+        tipo = TipoLancamentoEnum.RECEITA,
+        data = LocalDate.of(2024, 9, 5),
+        paga = true
+    ),
+    Lancamento(
+        descricao = "Aluguel",
+        valor = BigDecimal("1500.0"),
+        tipo = TipoLancamentoEnum.DESPESA,
+        data = LocalDate.of(2024, 9, 10),
+        paga = true
+    ),
+    Lancamento(
+        descricao = "Condomínio",
+        valor = BigDecimal("200.0"),
+        tipo = TipoLancamentoEnum.DESPESA,
+        data = LocalDate.of(2024, 9, 15),
+        paga = false
+    )
+)
